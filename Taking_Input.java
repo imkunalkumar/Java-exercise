4dd50a4830499a96e8de351d4c8ac52795db2831
Kunal kumar <53083851+imkunalkumar@users.noreply.github.com>
2020-01-11 12:43:29 +0530

    Add files via upload
    Taking Input From User In Java Using Scanner.
    
    Scanner is class in java.
    It is found in java.util package and used for obtaining user input of the primitive datatype(int,double) and string.

---
+++ Taking_Input.java
@@ -0,0 +1,12 @@
+package day1;
+import java.util.Scanner;
+public class Taking_Input {
+
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+System.out.println("enter any integer");
+int input_integer=in.nextInt();
+System.out.println("entered integer is:");
+System.out.println(input_integer);
+	}
+}
